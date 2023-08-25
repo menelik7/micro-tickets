081ebb9de6553b2c96c1227a2bf34632833548d2
menelik7 <menelikworku@gmail.com>
2023-08-25 08:29:43 +0200

    Setup MongoDB, mongoose, user model with signup process and password hashing

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,6 +1,8 @@
 import express from "express";
 import "express-async-errors";
 import { json } from "body-parser";
+import mongoose from "mongoose";
+
 import { currentUserRouter } from "./routes/current-user";
 import { signinRouter } from "./routes/signin";
 import { signoutRouter } from "./routes/signout";
@@ -22,8 +24,19 @@ app.all("*", async () => {
 
 app.use(errorHandler);
 
-const PORT = process.env.PORT || 3000;
+const start = async () => {
+	try {
+		await mongoose.connect("mongodb://auth-mongo-srv:27017/auth");
+		console.log("Connected to MongoDB");
+	} catch (err) {
+		console.error(err);
+	}
 
-app.listen(PORT, () => {
-	console.log("App listening on port", PORT);
-});
+	const PORT = process.env.PORT || 3000;
+
+	app.listen(PORT, () => {
+		console.log("App listening on port", PORT);
+	});
+};
+
+start();
