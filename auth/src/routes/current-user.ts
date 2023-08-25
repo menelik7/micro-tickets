311b7d5d390107e0e59c28d7a448ff707e11e7b9
menelik7 <menelikworku@gmail.com>
2023-08-25 14:02:49 +0200

    Handling auth verification with jwt and kubectl secret

--- auth/src/routes/current-user.ts
+++ auth/src/routes/current-user.ts
@@ -3,7 +3,7 @@ import express from "express";
 const router = express.Router();
 
 router.get("/api/users/currentuser", (req, res) => {
-	res.send("Hi there current user");
+	res.send("Hi there current user!!!");
 });
 
 export { router as currentUserRouter };
--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -3,6 +3,7 @@ import { body, validationResult } from "express-validator";
 import { User } from "../models/user";
 import { RequestValidationError } from "../errors/request-validation-error";
 import { BadRequestError } from "../errors/bad-request-error";
+import jwt from "jsonwebtoken";
 
 const router = express.Router();
 
@@ -32,6 +33,20 @@ router.post(
 
 		const user = User.build({ email, password });
 		await user.save();
+
+		// Generate json web token
+		const userJwt = jwt.sign(
+			{
+				id: user.id,
+				email: user.email,
+			},
+			process.env.JWT_KEY!
+		);
+
+		// Store it in our session object
+		req.session = {
+			jwt: userJwt,
+		};
 
 		res.status(201).send(user);
 	}
